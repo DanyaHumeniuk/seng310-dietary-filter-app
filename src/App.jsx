cabf4e1160d889d2714717ec6d7e807750893bb2
DanyaHumeniuk <dgumenuk076@gmail.com>
2025-07-03 15:09:23 -0700

    Started coding project

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
 
 
 const App = () => {
+
+
+  
   return (
-    <div>App</div>
+    <div>
+      
+
+      <header className="flex flex-row border-b-2 border-blue-950 justify-around items-center">
+        <h1 className="text-2xl ml-2">Title</h1>
+        <button className="text-lg bg-blue-400 rounded-md m-2 px-2">Home</button>
+      </header>
+
+      <div>
+
+      </div>
+
+
+    </div>
   )
 }
 
